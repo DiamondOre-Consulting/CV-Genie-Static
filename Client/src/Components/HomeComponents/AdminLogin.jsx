1485ffae5ba714fc38e3271162a7c1c13c491351
zoya774 <zoya.rasonline@gmail.com>
2024-05-17 16:28:30 +0530

    zoi

--- Client/src/Components/HomeComponents/AdminLogin.jsx
+++ Client/src/Components/HomeComponents/AdminLogin.jsx
@@ -4,14 +4,18 @@ import Footer from '../Commancomponents/Footer'
 import axios from "axios";
 import { useNavigate } from 'react-router-dom'
 
+
 const AdminLogin = () => {
     const navigate = useNavigate()
     const [userName, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [showLoader, setShowLoader] = useState(false);
+    const [error, setError] = useState(null);
 
     const handleAdminLogin = async (e) => {
         e.preventDefault();
-
+        setShowLoader(true);
         try {
             const response = await axios.post("https://cv-genie-static-backend.onrender.com/api/admin/login",
                 {
@@ -21,7 +25,7 @@ const AdminLogin = () => {
 
             if (response.status === 200) {
                 const token = response.data.token;
-
+                setShowLoader(false);
                 console.log(token)
                 localStorage.setItem("token", token);
                 console.log("Logged in successfully as Admin");
@@ -29,6 +33,7 @@ const AdminLogin = () => {
 
             } else {
                 console.log("Login failed");
+                setShowLoader(false);
                 setError("Login Details Are Wrong!!");
                 alert("invalid username and password")
                 // Handle login error
@@ -38,7 +43,8 @@ const AdminLogin = () => {
                 const status = error.response.status;
                 if (status === 401) {
                     console.log("invalid user");
-
+                    setError("Login Details Are Wrong!!");
+                    setShowLoader(false);
                 }
 
                 else {
@@ -50,6 +56,10 @@ const AdminLogin = () => {
     }
 
 
+    const handleShowPassword = () => {
+        return setShowPass(!showPass);
+    };
+
 
 
 
@@ -59,28 +69,48 @@ const AdminLogin = () => {
 
             <div class="min-h-screen flex items-center justify-center w-full dark:bg-gray-950">
                 <div class="bg-white dark:bg-gray-900 shadow-md rounded-lg px-8 py-6 max-w-md">
-                    <h1 class="text-2xl font-bold text-center mb-4 dark:text-gray-200">Admin login</h1>
-                    <form action="#" onSubmit={handleAdminLogin}>
+                    <h1 class="text-2xl font-bold text-center mb-4 dark:text-gray-200">Admin Login</h1>
+                    <form action="#" onSubmit={handleAdminLogin} className='md:w-96 w-56'>
                         <div class="mb-4">
                             <label for="name" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">User Name</label>
                             <input type="text" id="name" value={userName} onChange={(e) => setUsername(e.target.value)} class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="Enter your User Name" required />
                         </div>
                         <div class="mb-4">
                             <label for="password" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Password</label>
-                            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="Enter your password" required />
-                            <a href="#"
-                                class="text-xs text-gray-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Forgot
-                                Password?</a>
-                        </div>
-                        <div class="flex items-center justify-between mb-4">
-                            <div class="flex items-center">
-                                <input type="checkbox" id="remember" class="h-4 w-4 rounded border-gray-300 text-teal-900 focus:ring-indigo-500 focus:outline-none" checked />
-                                <label for="remember" class="ml-2 block text-sm text-gray-700 dark:text-gray-300">Remember me</label>
-                            </div>
+                            <input id="password" type={showPassword ? 'text' : 'password'} value={password} onChange={(e) => setPassword(e.target.value)} class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="Enter your password" required />
 
                         </div>
-                        <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-teal-900 hover:bg-teal-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-900">Login</button>
+                        <div class="flex items-center justify-between mb-4">
+                            <div class="flex items-start">
+                                <div class="flex items-center h-5">
+                                    <input aria-describedby="remember" id="check"
+                                        type="checkbox"
+                                        value={showPassword}
+                                        onChange={() =>
+                                            setShowPassword((prev) => !prev)
+                                        } class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300    " required="" />
+                                </div>
+                                <div class="ml-3 text-sm">
+                                    <label for="remember" class="text-gray-500 ">Show password</label>
+                                </div>
+                            </div>
+                            {/* <a href="#" class="text-sm font-medium text-primary-600 hover:underline ">Forgot password?</a> */}
+                        </div>
+                        <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-teal-900 hover:bg-teal-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-900"> {showLoader ? (
+                            <svg aria-hidden="true" class="inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                            </svg>
+                        ) : (
+                            <span class="relative z-10">Login</span>
+                        )}</button>
                     </form>
+
+                    {error && (
+                        <div className="flex items-center justify-center bg-red-300 p-4 rounded-m mt-4">
+                            <p className="text-center text-sm text-red-500">{error}</p>
+                        </div>
+                    )}
                 </div>
             </div>
 
--- Client/src/Components/HomeComponents/Checkout.jsx
+++ Client/src/Components/HomeComponents/Checkout.jsx
@@ -1,50 +1,77 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import Navbar from '../Commancomponents/Navbar';
 import Footer from '../Commancomponents/Footer';
-import axios from "axios";
+import axios from 'axios';
+import Typewriter from 'typewriter-effect/dist/core';
 
 const Checkout = () => {
-
+    
+    const typeRef = useRef(null);
     const [email, setEmail] = useState("");
     const [profile, setProfile] = useState(null);
     const [error, setError] = useState("");
-
-    // useEffect(() => {
-    //     if (email) {
-    //         handleSearch();
-    //     }
-    // }, [email]);
+    const [showLoader, setShowLoader] = useState(false);
+    
 
     const handleSearch = async () => {
         try {
-            const response = await axios.post(`https://cv-genie-static-backend.onrender.com/api/client/my-profile`, {email}
-            );
+            setShowLoader(true);
+            const response = await axios.post('https://cv-genie-static-backend.onrender.com/api/client/my-profile', { email });
             if (response.status === 200) {
-                console.log(response.data);
                 setProfile(response.data);
-                setError(""); // Clear any previous error
+                console.log(response.data)
+                setError("");
             } else {
-                setError("No data found"); // Set error message if response or data is empty
-                // setProfile(null);
+                setError("No data found");
+                setProfile(null);
             }
         } catch (error) {
             console.error("Error:", error);
-            setError(error.response?.data?.message || "An error occurred"); // Use custom error message if available
-            // setProfile(null);
+            setError(error.response?.data?.message || "An error occurred");
+            setProfile(null);
+        } finally {
+            setShowLoader(false);
         }
     };
 
+
+    useEffect(() => {
+        const typewriter = new Typewriter(typeRef.current, {
+            loop: true,
+            delay: 75,
+        });
+
+        typewriter
+            .typeString('<span class="text-teal-800">R</span>eady To')
+            .pauseFor(500)
+            .typeString(' Invest In')
+            .pauseFor(500)
+            .typeString(' Your ')
+            .pauseFor(500)
+            .typeString('<span class="text-green-800 underline"> Career</span> !!!')
+            .pauseFor(2000)
+            .deleteAll()
+            .start();
+
+        return () => {
+            typewriter.stop();
+        };
+    }, []);
+
+
     return (
-        <div className='bg-green-50'>
-        <Navbar/>
-            <div className="container mx-auto min-h-screen  py-8">
+        <div className='bg-green-50 min-h-screen flex flex-col'>
+            <Navbar />
+            <div className="container mx-auto flex-grow py-8">
                 <h1 className="text-3xl text-center font-bold mb-4">CHECK OUT</h1>
-                <div className="flex justify-center items-center  mb-4">
+                <p ref={typeRef} className='text-3xl text-center mb-4'></p>
+                <div className="flex justify-center items-center mb-4">
                     <input
                         type="text"
                         placeholder="Search by Email"
                         className="px-4 py-2 border border-gray-300 rounded-l-md focus:outline-none"
-                        value={email} onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                     <button
                         className="px-4 py-2 bg-teal-900 text-white rounded-r-md hover:bg-teal-800 focus:outline-none"
@@ -53,32 +80,40 @@ const Checkout = () => {
                         Search
                     </button>
                 </div>
-                {error && <p>{error}</p>}
-                {profile && (
-                    <div className=" rounded-md  mx-auto text-gray-800 bg-gray-50 border w-fit">
-                        <div className='pl-4 pr-24 text-xl py-4 mt-2'>
-                        <p><span className="font-semibold mb-1">Name :</span> {profile.name}</p>
-                        <p><span className="font-semibold mb-1">Email :</span> {profile.email}</p>
-                        <p><span className="font-semibold mb-1">Phone :</span> {profile.phone}</p>
-                        <p><span className="font-semibold mb-1">Total Amount :</span> {profile.amount}</p>
-                        
-                        </div>
-                        {profile.ispaid ? (
-                            <button className='bg-green-600 hover:bg-green-700 text-gray-100 w-full !px-0 mt-4 p-4'>
-                                Paid
-                            </button>
-                        ) : (
-                            <button className='bg-red-600 hover:bg-red-700 text-gray-100 w-full !px-0 mt-4 p-4' >
-                                Pay Now {profile.amount}
-                            </button>
-                        )}
+
+                {error && <p className="text-red-500 text-center">{error}</p>}
+                {showLoader ? (
+                    <div className="flex justify-center items-center">
+                        <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                        </svg>
                     </div>
+                ) : (
+                    profile && (
+                        <div className="rounded-md mx-auto text-gray-800 bg-gray-50 border w-fit">
+                            <div className='pl-4 pr-24 text-xl py-4 mt-2'>
+                                <p><span className="font-semibold mb-1">Name :</span> {profile.name}</p>
+                                <p><span className="font-semibold mb-1">Email :</span> {profile.email}</p>
+                                <p><span className="font-semibold mb-1">Phone :</span> {profile.phone}</p>
+                                <p><span className="font-semibold mb-1">Total Amount :</span> {profile.amount}</p>
+                            </div>
+                            {profile.paid ? (
+                                <button className='bg-green-600 hover:bg-green-700 text-gray-100 w-full disable cursor-not-allowed !px-0 mt-4 p-4'>
+                                        You Have Already Paid
+                                </button>
+                            ) : (
+                                <button className='bg-red-600 hover:bg-red-700 text-gray-100 w-full !px-0 mt-4 p-4'>
+                                    Pay Now {profile.amount}
+                                </button>
+                            )}
+                        </div>
+                    )
                 )}
             </div>
-
-            <Footer/>
+            <Footer />
         </div>
-    )
+    );
 }
 
-export default Checkout
+export default Checkout;
